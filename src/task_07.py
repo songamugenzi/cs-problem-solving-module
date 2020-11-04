992b87051ecf7a054e9109d694346fdf0b6a52d9
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-11-04 15:12:48 -0500

    Completed task 01

---
+++ src/task_07.py
@@ -0,0 +1,17 @@
+"""
+What are the four steps in the UPER problem-solving framework?
+"""
+
+
+# Answer
+"""
+The U.P.E.R problem-solving framework involves these four steps:
+
+- *Understand*: understand exactly what the problem is asking
+
+- *Plan*: take your description of the problem and transform it into a complete, actionable plan to solve that problem 
+
+- *Execute*: convert your plan into actual working code
+
+- *Reflect*: improve implementation, polish, and improve your solution to be as performant as it can be
+"""
