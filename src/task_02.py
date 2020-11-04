0bafbebe654359ddd8b993689bb95f5a30b76bbd
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-11-04 15:16:02 -0500

    Completed task 02

---
+++ src/task_02.py
@@ -0,0 +1,9 @@
+"""
+What is the most important action to take during the Plan step of UPER?
+"""
+
+
+# Answer
+"""
+Taking the problem description and transforming it into a complete, actionable plan to solve that problem (often using pseudocode to do so).
+"""
